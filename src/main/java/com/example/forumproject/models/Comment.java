c5ba916982bdfb890fe92c6717b959e602801269
petkoAs95 <petko.asenov.a60@learn.telerikacademy.com>
2024-07-20 19:37:17 +0300

    Implementation of Comment persistent class.

--- src/main/java/com/example/forumproject/models/Comment.java
+++ src/main/java/com/example/forumproject/models/Comment.java
@@ -1,9 +1,123 @@
 package com.example.forumproject.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "comments")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "creator")
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User creator;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.creationDate = LocalDateTime.now();
+    }
+
+    public Comment() {
+    }
+
+    public Comment(int id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return id == comment.id && Objects.equals(content, comment.content) && Objects.equals(creator, comment.creator) && Objects.equals(creationDate, comment.creationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content, creator, creationDate);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
